c2457499736c57b0829c41d7521162b6e25204af
enoreex <icenetovec@gmail.com>
2025-01-18 18:26:33 +0200

    fixed static

--- src/ice.rudp/common/rudp.cpp
+++ src/ice.rudp/common/rudp.cpp
@@ -2,6 +2,9 @@
 
 #pragma region SCHEDULER
 
+bool scheduler::use_cached_time = false;
+std::chrono::time_point<std::chrono::system_clock> scheduler::cached_time = std::chrono::system_clock::now();
+
 bool scheduler::empty()
 {
     return head == nullptr ? true : false;
